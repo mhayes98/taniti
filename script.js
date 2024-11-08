0a42083edcd4624ecca3a9248871b1475b7d7c1d
matt <mh.dev@pm.me>
2024-11-07 23:38:33 -0500

    Header work

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const search_icon = document.querySelector("#search-icon");
-search_icon.addEventListener("click", () => {
-    const search_container = document.createElement("div");
-    const header = document.querySelector(".header-nav");
-    const search_bar = document.createElement("form");
-    const search_input = document.createElement("input");
-    search_input.placeholder = "Search...";
-
-    search_container.appendChild(search_bar);
-    search_bar.appendChild(search_input);
-
-    search_container.classList.add("search_container");
-    header.appendChild(search_container);
+const search = document.querySelector(".search");
+search.addEventListener("click", () => {
+    navToggle();
+    buttonToggle();
 })
 
+function navToggle(){
+    const search_bar = document.querySelector("#search-wrapper");
+    const nav_bar_links = document.querySelector("#header-nav-bar-links");
+
+    search_bar.style.display = search_bar.style.display == "none" ? "flex" : "none";
+    nav_bar_links.style.display = search_bar.style.display == "none" ? "flex" : "none";
+}
+
+function buttonToggle(){
+    const search_icon = document.querySelector(".fa-search.rs");
+    const close_icon = document.querySelector(".fa-close.rs");
+
+    search_icon.style.display = search_icon.style.display == "none" ? "flex" : "none";
+    close_icon.style.display = close_icon.style.display == "none" ? "flex" : "none";
+}
+
+// Idea for mobile compat : when hamburger present --> hide 'Taniti' h1 (when searching)
