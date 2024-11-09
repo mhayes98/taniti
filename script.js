4d8ebed9f0b92ca1a29648b31fd7bee30d7d7cbb
matt <mh.dev@pm.me>
2024-11-09 13:08:41 -0500

    Cont. header work

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const search = document.querySelector(".search");
 search.addEventListener("click", () => {
     navToggle();
-    buttonToggle();
+    buttonToggle(); 
 })
 
 function navToggle(){
     const search_bar = document.querySelector("#search-wrapper");
     const nav_bar_links = document.querySelector("#header-nav-bar-links");
 
-    search_bar.style.display = search_bar.style.display == "none" ? "flex" : "none";
-    nav_bar_links.style.display = search_bar.style.display == "none" ? "flex" : "none";
+    // Adds class to toggle slide-in vs slide-out animation
+    if(search_bar.style.display == "flex"){
+        search_bar.classList.add("hide-animation");
+        search_bar.classList.remove("show-animation");
+        setTimeout(() => {
+            search_bar.style.display = "none";
+        }, 200);
+    }
+    else{
+        search_bar.classList.remove("hide-animation");
+        search_bar.classList.add("show-animation");
+        search_bar.style.display = "flex";
+    }
+    
+    // Adds class to toggle fade-in animation (post slide-out above)
+    if(nav_bar_links.style.display == "none"){
+        setTimeout(() => {
+            nav_bar_links.classList.add("fade-in");
+            nav_bar_links.style.display = "flex";
+        }, 250);
+    }
+    else{
+        nav_bar_links.style.display = "none";
+        nav_bar_links.classList.remove("fade-in");
+    }
 }
 
 function buttonToggle(){
@@ -21,3 +44,4 @@ function buttonToggle(){
 }
 
 // Idea for mobile compat : when hamburger present --> hide 'Taniti' h1 (when searching)
+
